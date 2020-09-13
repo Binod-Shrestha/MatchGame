9eaa09800a297872ab7ed13464a641d4149edfbb
shrbinod <shresthabinod.softdev@gmail.com>
2020-09-13 16:44:14 -0400

    list of items are displayed

--- MatchGame/ViewController.swift
+++ MatchGame/ViewController.swift
@@ -8,14 +8,18 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-   
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     let model = CardModel()
     var cardArray = [Card]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        collectionView.delegate = self
+        collectionView.dataSource = self
         
         // call the getCards of the card model
         cardArray = model.getCards()
@@ -23,6 +27,20 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
+    
+    //MARK: - UICollectionView Protocol Methods
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return cardArray.count
+        
+    }
 
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CardCell", for: indexPath)
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+    }
 }
 
